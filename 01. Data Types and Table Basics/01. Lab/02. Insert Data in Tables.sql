ca1e1e93d7bfecbe8357d633d1e4df7ebe726e13
Dimitar-Peev <mctrix87@abv.bg>
2026-04-28 15:04:57 +0300

    Add SQL script to insert sample data into "employees" table

---
+++ 01. Data Types and Table Basics/01. Lab/02. Insert Data in Tables.sql
@@ -0,0 +1,4 @@
+INSERT INTO "employees"("first_name", "last_name", "hiring_date", "salary", "devices_number")
+VALUES ('Ivan', 'Ivanov', '2026-01-05', 620.00, 100),
+       ('Petar', 'Petrov', '2026-01-06', 630.00, 200),
+       ('Dimitar', 'Dimitrov', '2026-01-07', 640.00, 300);
