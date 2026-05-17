d5270d567349896ba0c5f87e80a8ca1de20744aa
Dimitar-Peev <mctrix87@abv.bg>
2026-05-17 09:45:15 +0300

    Solution - Task 3

---
+++ 03. Built-in Functions/02. Exercise/03. Capital Code.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "countries"
+    ADD COLUMN "capital_code" CHAR(2);
+
+UPDATE "countries"
+SET "capital_code" = SUBSTRING("capital" FROM 1 FOR 2);
+
+SELECT "capital_code"
+FROM "countries";
