86697e84ca99e7c3012bcaf5a4e63089b45e053a
Dimitar-Peev <mctrix87@abv.bg>
2026-05-28 07:12:22 +0300

    Solution - Task 12

--- 05. Table Relations/02. Exercise/12. Update Cascade.sql
+++ 05. Table Relations/02. Exercise/12. Update Cascade.sql
@@ -1,6 +1,6 @@
 ALTER TABLE "countries_rivers"
 
-    ADD CONSTRAINT k_countries_rivers_rivers
+    ADD CONSTRAINT fk_countries_rivers_rivers
         FOREIGN KEY ("river_id")
             REFERENCES "rivers" ("id")
             ON UPDATE CASCADE,
