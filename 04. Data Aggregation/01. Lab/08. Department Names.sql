c784ea5f90272946f6a7c949bfff7d191e155697
Dimitar-Peev <mctrix87@abv.bg>
2026-06-02 05:28:36 +0300

    Solution - Task 8

---
+++ 04. Data Aggregation/01. Lab/08. Department Names.sql
@@ -0,0 +1,18 @@
+SELECT "id",
+       "first_name",
+       "last_name",
+       TRUNC("salary", 2) AS "salary",
+       "department_id",
+--        CASE
+--            WHEN department_id = 1 THEN 'Management'
+--            WHEN department_id = 2 THEN 'Kitchen Staff'
+--            WHEN department_id = 3 THEN 'Service Staff'
+--            ELSE 'Other'
+       CASE "department_id"
+           WHEN 1 THEN 'Management'
+           WHEN 2 THEN 'Kitchen Staff'
+           WHEN 3 THEN 'Service Staff'
+           ELSE 'Other'
+           END            AS "department_name"
+FROM "employees"
+ORDER BY "id";
