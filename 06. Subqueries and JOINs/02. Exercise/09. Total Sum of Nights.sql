097959412b16c6486e89537ed900d53f3dda4413
Dimitar-Peev <mctrix87@abv.bg>
2026-06-11 05:43:52 +0300

    Solution - Task 9

---
+++ 06. Subqueries and JOINs/02. Exercise/09. Total Sum of Nights.sql
@@ -0,0 +1,7 @@
+SELECT a."name",
+       SUM(b."booked_for") AS "sum"
+FROM "apartments" AS a
+         INNER JOIN "bookings" AS b
+                    USING ("apartment_id")
+GROUP BY a."name"
+ORDER BY a."name";
