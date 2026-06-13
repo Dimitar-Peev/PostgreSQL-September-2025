32d6f71e1d58b322c95dfb17add73875939d3bf6
Dimitar-Peev <mctrix87@abv.bg>
2026-06-13 11:20:51 +0300

    Solution - Task 5

---
+++ 09. Regular Exam - 13 June 2026/05. Customers.sql
@@ -0,0 +1,8 @@
+SELECT "id",
+       "last_name",
+       "loyalty_card"
+FROM "customers"
+WHERE ("last_name" LIKE '%m%' OR "last_name" LIKE '%M%')
+  AND "loyalty_card" = TRUE
+ORDER BY "last_name" DESC,
+         "id";
